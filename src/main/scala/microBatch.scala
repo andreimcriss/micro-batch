5b456dc068aa4505c457cc5494dee66089a75dee
Andrei Muraru <andrei.cristian.muraru@gmail.com>
2016-10-03 18:51:31 +0300

    Update microBatch.scala

--- src/main/scala/microBatch.scala
+++ src/main/scala/microBatch.scala
@@ -1,12 +1,7 @@
-import sqlContext.implicits._
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.streaming.Duration
-import org.apache.spark.streaming.Seconds
 import org.apache.spark.SparkConf
-
+import java.util.TimerTask
+import java.util.Timer
 
 object microBatch{
 
@@ -16,42 +11,40 @@ object microBatch{
         |Usage: microBatch <source_locations> <microbatch_interval> <queries_file> <output_tables_file>
         |  <source_location> is the HDFS path where the Parquet files are located
         |  <microbatch_interval> is the amount of time in seconds between two succesive runs of the job
-		""".stripMargin)
+                """.stripMargin)
       System.exit(1)
     }
-   
-     //Create SparkContext
-    val conf = new SparkConf()
-      .setMaster("yarn-client")
-      .setAppName("MicroBatch")
-      .set("spark.executor.memory", "5g")
-      .set("spark.rdd.compress","true")
-      .set("spark.storage.memoryFraction", "1")
-      .set("spark.streaming.unpersist", "true")
 
-    val Array(source_location,interval) = args
+      //.set("spark.storage.memoryFraction", "1")
+      //.set("spark.streaming.unpersist", "true")
 
+        val Array(source_location) = args
 
-    val sparkConf = new SparkConf().setAppName("MicroBatch")
-    val sc = new SparkContext(sparkConf)
-	
-	val ssc = SparkSession.builder().appName("microbatch").config("spark.sql.warehouse.dir","hdfs:///user/hive/warehouse/").enableHiveSupport().getOrCreate()
-	//SparkSQL Hive Context
-	
-    
-	val source_schema = spark.read.load(source_location).schema
-	val stream_session = ssc.readStream.schema(source_schema).parquet(source_location)
-	
-	//insert query here using stream_session dataframe
+        val spark = SparkSession.builder().master("yarn").appName("microbatch").config("spark.sql.warehouse.dir","hdfs:///user/hive/warehouse/").enableHiveSupport().getOrCreate()
+        spark.conf.set("spark.executor.memory", "5g")
+        spark.conf.set("spark.rdd.compress","true")
 
-	val result_query = stream_session.select("customers_city").groupBy("customers_city").count()
+        val source_schema = spark.read.load(source_location).schema
+        val stream_session = spark.readStream.schema(source_schema).parquet(source_location)
 
-	//write to Hive
-	val write_query = result_query.writeStream.outputMode("complete").format("memory").queryName("test_micro_sparksubmit").start()
+        //insert query here using stream_session dataframe
 
-	write_query.awaitTermination()
+        val result_query = stream_session.select("customers_city").groupBy("customers_city").count()
+
+        //write to memory table
+        val write_query = result_query.writeStream.outputMode("complete").format("memory").queryName("test_micro_sparksubmit").start()
 
 
-	}
+//val write_query = result_query.writeStream.outputMode("complete").format("console").queryName("test_micro_sparksubmit").start()
+
+        val timer = new Timer("rewrite table", true)
+        timer.schedule(new TimerTask{
+                override def run() {
+                        spark.sql("SELECT * FROM test_micro_sparksubmit").write.format("parquet").mode("overwrite").saveAsTable("microbatch_table")
+                }
+        }, 30000, 15000)
+
+        write_query.awaitTermination()
+        }
 
 }
